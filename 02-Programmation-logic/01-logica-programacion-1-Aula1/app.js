39fdb644e4a59d577d8a77d2aee08cf2ee40c050
Pol Castillo <rpcastilloy@gmail.com>
2024-06-20 22:29:19 -0500

    finalizacion de primer curso de logica de programacion

--- 02-Programmation-logic/01-logica-programacion-1-Aula1/app.js
+++ 02-Programmation-logic/01-logica-programacion-1-Aula1/app.js
@@ -1,11 +1,12 @@
-let numeroSecreto = 6;
-let numeroUsuario = null;
+let numeroLimite = parseInt(prompt("Ingrese numero limite para empezar a jugar"));
+let numeroSecreto = Math.floor(Math.random() * numeroLimite) + 1;
+let numeroUsuario = 0;
 let contador = 1;
-let maxIntentos = 3;
+let maxIntentos = 5;
 
 while (numeroSecreto != numeroUsuario) {
   numeroUsuario = parseInt(
-    prompt("Me indicas un numero entre 1 y 10 por favor:")
+    prompt(`Me indicas un numero entre 1 y ${numeroLimite} por favor:`)
   );
 
   if (numeroUsuario === numeroSecreto) {
@@ -21,9 +22,8 @@ while (numeroSecreto != numeroUsuario) {
       alert("No acertaste, el numero que ingresaste es mayor");
     }
     contador++;
-    palabraVeces = "veces";
 
-    if (contador > 3) {
+    if (contador > maxIntentos) {
       alert(`Tus intentos terminaron, son solo ${maxIntentos} veces`);
       break;
     }
