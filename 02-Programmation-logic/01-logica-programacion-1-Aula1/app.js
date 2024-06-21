458ab4510fd6901ca2bac51e387b2754e7fcb9aa
Pol Castillo <rpcastilloy@gmail.com>
2024-06-20 20:44:23 -0500

    Update project

--- 02-Programmation-logic/01-logica-programacion-1-Aula1/app.js
+++ 02-Programmation-logic/01-logica-programacion-1-Aula1/app.js
@@ -1,8 +1,16 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+
+
+if (numeroUsuario === numeroSecreto) {
+    alert(`Acertaste, el numero es: ${numeroUsuario}`);
+} else {
+    if(numeroUsuario < numeroSecreto) {
+        alert('No acertaste, el numero que ingresaste es menor');
+    } else if (numeroUsuario > numeroSecreto) {
+        alert('No acertaste, el numero que ingresaste es mayor');
+    } 
 }
