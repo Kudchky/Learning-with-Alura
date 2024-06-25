70c947c1954d7880e68e4c121c59fa39b81007d2
Pol Castillo <rpcastilloy@gmail.com>
2024-06-24 22:08:21 -0500

    Finalizando el desafio

--- 03-Programmation-logic-II/02-js-curso-2-main/app.js
+++ 03-Programmation-logic-II/02-js-curso-2-main/app.js
@@ -1,1 +1,21 @@
+document.querySelector("h1").innerText = "Hora del Desafio";
 
+function consola() {
+  console.log("El botón fue clicado");
+}
+
+const promptFunc = () => {
+  let city = prompt("Dime una ciudad de Brasil");
+  alert(`Estuve en ${city} y me acorde de ti`);
+}
+
+const alerta = () => {
+  alert("Yo amo JS" );
+}
+
+const suma = () => {
+  let num1 = parseInt(prompt("Ingresa primer numero"));
+  let num2 = parseInt(prompt("Ingrese segundo numero"));
+
+  alert(`La suma es: ${num1 + num2}`);
+}
